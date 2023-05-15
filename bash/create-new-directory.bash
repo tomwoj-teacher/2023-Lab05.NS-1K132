93eadc5504a8d568415cfd1f2ee583289035e426
Natalia <nkrzyszt@student.wszib.edu.pl>
2023-05-15 12:49:22 +0200

    Minor changes as recommended.

--- bash/create-new-directory.bash
+++ bash/create-new-directory.bash
@@ -2,4 +2,10 @@
 #Natalia Krzysztof
 
 read -p "Enter the name of the new directory: " directory_name
-mkdir "$directory_name"
+
+if [ -d "$directory_name" ]; then
+	echo "Directory $directory_name already exists."
+else
+	sudo mkdir "$directory_name"
+	echo "Directory has been successfully created."
+fi
